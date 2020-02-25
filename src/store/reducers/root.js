cab20b05929c7bd8df2c4df6eccc88cf3d5c680a
Tornike Shavidze <shavidzet@protonmail.com>
2020-02-25 17:15:42 +0400

    integrate redux storage

---
+++ src/store/reducers/root.js
@@ -0,0 +1,16 @@
+import faker from 'faker';
+const initialState = [] 
+
+const root = (state = initialState, action ) => {
+    switch (action.type) {
+        case 'ADD_TEST':
+            return [
+                ...state,
+                faker.name.findName()
+            ]
+        default:
+            return state
+    }
+}
+
+export default root
