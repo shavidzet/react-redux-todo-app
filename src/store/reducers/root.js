8154bcb28578060ff69efb204d1ea02e5e0dffa9
Tornike Shavidze <shavidzet@protonmail.com>
2020-02-25 17:21:27 +0400

    fixed by eslint

--- src/store/reducers/root.js
+++ src/store/reducers/root.js
@@ -1,16 +1,16 @@
-import faker from 'faker';
-const initialState = [] 
+import faker from 'faker'
+const initialState = []
 
-const root = (state = initialState, action ) => {
-    switch (action.type) {
-        case 'ADD_TEST':
-            return [
-                ...state,
-                faker.name.findName()
-            ]
-        default:
-            return state
-    }
+const root = (state = initialState, action) => {
+  switch (action.type) {
+    case 'ADD_TEST':
+      return [
+        ...state,
+        faker.name.findName()
+      ]
+    default:
+      return state
+  }
 }
 
 export default root
